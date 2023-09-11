22f90455b05e9e2c9cadacbb98416b4be07ac521
Keita Nakamura <keita.nakamura.1109@gmail.com>
2023-09-11 15:04:51 +0900

    Add julia_main function for PackageCompiler.jl

--- src/PileWave.jl
+++ src/PileWave.jl
@@ -10,4 +10,19 @@ include("input.jl")
 include("states.jl")
 include("solver.jl")
 
+function julia_main()::Cint
+    if isempty(ARGS)
+        inputtoml = "input.toml"
+    else
+        inputtoml = ARGS[1]
+    end
+    try
+        solve(inputtoml)
+    catch
+        Base.invokelatest(Base.display_error, Base.catch_stack())
+        return 1
+    end
+    return 0
+end
+
 end # module PileWave
